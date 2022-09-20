49f74db53fa99f90ebe76d28f26d660fe18b51c7
chanu2 <mdsoo55828@gmail.com>
2022-09-20 21:07:37 +0900

    API

---
+++ src/main/java/jpabooks/jpastore/api/MemberApiController.java
@@ -0,0 +1,49 @@
+package jpabooks.jpastore.api;
+
+import jpabooks.jpastore.domain.Member;
+import jpabooks.jpastore.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    //별도의 dto를 만들어서 파라미터로 받는 것이 좋다!! 엔티티를 직접 바인딩 받아서 사용하는 것은 좋은 방법이 아니다.
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+
+    //Dto를 만들어서 사용한다
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest{
+        private String name;
+
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
